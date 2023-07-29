7ff1821379fbba07c3701056ab167c39be1ef56a
KatrinaWright <katrina.elise.wright@gmail.com>
2023-07-29 12:55:42 -0400

    kinda roolling dice

--- joy-game/src/components/Dice.tsx
+++ joy-game/src/components/Dice.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
-
-import "./Dice.css"
-import dice1 from "../assets/pngs/dice1.png"
-import dice2 from "../assets/pngs/dice2.png"
-import dice3 from "../assets/pngs/dice3.png"
-import dice4 from "../assets/pngs/dice4.png"
-import dice5 from "../assets/pngs/dice5.png"
-import dice6 from "../assets/pngs/dice6.png"
-
+import { motion } from 'framer-motion';
+import './Dice.css';
+import dice1 from '../assets/pngs/dice1.png';
+import dice2 from '../assets/pngs/dice2.png';
+import dice3 from '../assets/pngs/dice3.png';
+import dice4 from '../assets/pngs/dice4.png';
+import dice5 from '../assets/pngs/dice5.png';
+import dice6 from '../assets/pngs/dice6.png';
 
 interface DiceProps {
   faceValue: number;
 }
 
 const Dice: React.FC<DiceProps> = ({ faceValue }) => {
+  const dices = [dice1, dice2, dice3, dice4, dice5, dice6];
 
-  const dices = [dice1,dice2,dice3,dice4,dice5,dice6]
-
+  // Rotation for each dice face based on the index (0 to 5)
+  const rotations = [0, -90, 180, -270, 360, -450];
 
   return (
-    <>
-
-        <div  className='img-container'> <img  src={dices[faceValue-1]}></img></div>
-        
-      
-      
-    </>
-  )
+    <motion.div
+      className={`dice dice-${faceValue}`}
+      initial={{ rotate: 0 }}
+      animate={{
+        rotate: rotations[faceValue - 1],
+        transition: { duration: 1.5 },
+      }}
+    >
+      {[...Array(6)].map((_, i) => (
+        <div key={i} className={`side side-${i + 1}`}>
+          <div className="img-container">
+            <img src={dices[i]} alt={`Dice ${i + 1}`} />
+          </div>
+        </div>
+      ))}
+    </motion.div>
+  );
 };
 
 export default Dice;
-
-// file composed by chatgpt; inserted by euphina
--- joy-game/src/components/Table.tsx
+++ joy-game/src/components/Table.tsx
@@ -1,37 +1,23 @@
-import {motion} from "framer-motion";
-import Dice from "./Dice.tsx";
+//import {motion} from "framer-motion";
 import React from "react";
-import {GameState} from "../logic.ts";
-import "./Table.css"
+import Dice from "./Dice.tsx";
+import { GameState } from "../logic.ts";
+import "./Table.css";
 
 interface TableProps {
-    game: GameState,
-
+  game: GameState;
 }
-const Table:React.FC<TableProps> = ({game}) => (
 
-    <div className='middle-section'>
-
-
-
-       
-
-        <div className='dice-container'>
-
-            
-            {game.gameDice.map((die, i )=>(
-                <motion.button transition={{ duration: 1.3 }} animate={{
-                    scale: [1, 2, 2, 1, 1],
-                    rotate: [0, 0, 270, 270, 0],
-
-                }}
-                               className='dice-button' key={i} ><Dice faceValue={die} /></motion.button>))}
+const Table: React.FC<TableProps> = ({ game }) => (
+  <div className="middle-section">
+    <div className="dice-container">
+      {game.gameDice.map((die, i) => (
+        <div key={i} className="dice-button">
+          <Dice faceValue={die} />
         </div>
-
-
-        
-
+      ))}
     </div>
+  </div>
 );
 
 export default Table;
